f1387ac65687a5fe98fbd27eadb84387f2ee69bf
Eddykasp <maxkasperowski@gmail.com>
2018-01-11 14:15:26 +0800

    change indentation to 2 spaces

--- calculator.js
+++ calculator.js
@@ -1,32 +1,31 @@
 
 function calculate (){
-    canvas = document.getElementById('output-canvas');
-    ctx = canvas.getContext('2d');
-    let sides = parseFloat(document.getElementById('sides-number').value);
-    let length = parseFloat(document.getElementById('side-length').value);
-    let angle = parseFloat(document.getElementById('start-degree').value);
+  canvas = document.getElementById('output-canvas');
+  ctx = canvas.getContext('2d');
+  let sides = parseFloat(document.getElementById('sides-number').value);    let length = parseFloat(document.getElementById('side-length').value);
+  let angle = parseFloat(document.getElementById('start-degree').value);
 
-    let vector = {x: length, y: 0.0};
-    let vertices = [];
-    let totalAngle = (sides-2)*180;
-    let innerAngle = totalAngle/sides;
-    console.log('sides: ' + sides + ' total angle: ' + totalAngle + ' inner angle: ' + innerAngle);
-    vertices.push({x: 0.0, y: 0.0});
-    console.log(vertices);
-    for(let i=0;i<sides-1;i++){
-        // add next vector
-        vertices.push({x: vertices[i].x+vector.x, y: vertices[i].y+vector.y});
-        // rotate for next iteration
-        let tmpX = vector.x*Math.cos(toRadians(180-innerAngle)) - vector.y*Math.sin(toRadians(180-innerAngle));
-        tmpX = Math.round(tmpX * 1000)/1000;
-        //console.log(vector.x*Math.cos(toRadians(innerAngle)) - vector.y*Math.sin(toRadians(innerAngle)));
-        let tmpY = vector.x*Math.sin(toRadians(180-innerAngle)) + vector.y*Math.cos(toRadians(180-innerAngle));
-        tmpY = Math.round(tmpY * 1000)/1000;
-        vector.y = tmpY;
-        vector.x = tmpX;
-    }
-    draw(vertices, ctx, canvas, length);
-    vertexTable(vertices);
+  let vector = {x: length, y: 0.0};
+  let vertices = [];
+  let totalAngle = (sides-2)*180;
+  let innerAngle = totalAngle/sides;
+  console.log('sides: ' + sides + ' total angle: ' + totalAngle + ' inner angle: ' + innerAngle);
+  vertices.push({x: 0.0, y: 0.0});
+  console.log(vertices);
+  for(let i=0;i<sides-1;i++){
+      // add next vector
+      vertices.push({x: vertices[i].x+vector.x, y: vertices[i].y+vector.y});
+      // rotate for next iteration
+      let tmpX = vector.x*Math.cos(toRadians(180-innerAngle)) - vector.y*Math.sin(toRadians(180-innerAngle));
+      tmpX = Math.round(tmpX * 1000)/1000;
+      //console.log(vector.x*Math.cos(toRadians(innerAngle)) - vector.y*Math.sin(toRadians(innerAngle)));
+      let tmpY = vector.x*Math.sin(toRadians(180-innerAngle)) + vector.y*Math.cos(toRadians(180-innerAngle));
+      tmpY = Math.round(tmpY * 1000)/1000;
+      vector.y = tmpY;
+      vector.x = tmpX;
+  }
+  draw(vertices, ctx, canvas, length);
+  vertexTable(vertices);
 }
 
 function toRadians (angle) {
